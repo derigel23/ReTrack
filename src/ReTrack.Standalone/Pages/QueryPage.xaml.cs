eb29352f17f2a28b0bb952c06735d5fe77f76601
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-05-06 20:11:00 +0400

    what? more?

---
+++ src/ReTrack.Standalone/Pages/QueryPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReTrack.Standalone.Pages
+{
+  /// <summary>
+  /// Interaction logic for QueryPage.xaml
+  /// </summary>
+  public partial class QueryPage : Page
+  {
+    public QueryPage()
+    {
+      InitializeComponent();
+    }
+
+    private void BtnSettings_OnClick(object sender, RoutedEventArgs e)
+    {
+      if (NavigationService != null)
+        NavigationService.Navigate(new Uri(@"Pages\SettingsPage.xaml", UriKind.RelativeOrAbsolute));
+    }
+  }
+}
