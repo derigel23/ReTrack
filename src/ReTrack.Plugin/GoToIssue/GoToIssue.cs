63e5d65db43fc841a8f7dac8635e076ba053c6da
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-08-09 15:43:58 +0400

    sample for kirill

---
+++ src/ReTrack.Plugin/GoToIssue/GoToIssue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.ActionManagement;
+using JetBrains.Application.DataContext;
+using JetBrains.ReSharper.Feature.Services.ContextNavigation;
+using JetBrains.ReSharper.Features.Common.Occurences.ExecutionHosting;
+using JetBrains.ReSharper.Features.Finding.NavigateFromHere;
+
+namespace ReTrack.GoToIssue
+{
+  [ActionHandler(ID)]
+  public class GoToIssue : ContextSearchActionBase<IssueSearchProvider>
+  {
+    private const string ID = "ReSharper.ReSharper_GoToIssue";
+  }
+
+  public class IssueSearchProvider : IContextSearchProvider
+  {
+    public Action GetSearchesExecution(IDataContext dataContext, INavigationExecutionHost host)
+    {
+      return () =>
+      {
+        {
+        }
+      };
+    }
+  }
+}
