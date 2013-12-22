e3e7e38e76afd02e1ff9488fcfa65d56207780c8
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-12-22 20:41:01 +0400

    updates to options page

--- src/ReTrack.Plugin/ReTrackOptionsPage.xaml.cs
+++ src/ReTrack.Plugin/ReTrackOptionsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using JetBrains.DataFlow;
 using JetBrains.ReSharper.Features.Common.Options;
 using JetBrains.UI.CrossFramework;
@@ -11,72 +13,109 @@ using ReTrack.Resources;
 
 namespace ReTrack
 {
-    [OptionsPage(PID, "ReTrack", typeof(ReTrackThemedIcons.YouTrack), ParentId = ToolsPage.PID)]
-    public partial class ReTrackOptionsPage : UserControl, IOptionsPage
+  [OptionsPage(PID, "ReTrack", typeof(ReTrackThemedIcons.YouTrack), ParentId = ToolsPage.PID)]
+  public partial class ReTrackOptionsPage : UserControl, IOptionsPage
+  {
+    private const string PID = "ReTrack.OptionsPage";
+
+    public ReTrackOptionsPage(Lifetime lifetime, OptionsSettingsSmartContext ctx)
     {
-        private const string PID = "ReTrack.OptionsPage";
+      InitializeComponent();
 
-        public ReTrackOptionsPage(Lifetime lifetime, OptionsSettingsSmartContext ctx)
-        {
-            InitializeComponent();
-
-            ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackUsername, UsernameBox, TextBox.TextProperty);
-            ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackPassword, PasswordBox, TextBox.TextProperty);
-            ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackUrl, UrlBox, TextBox.TextProperty);
-        }
-
-        public bool OnOk()
-        {
-            return true;
-        }
-
-        public bool ValidatePage()
-        {
-            // todo: attempt to connect to source (if specified)
-            return true;
-        }
-
-        public EitherControl Control
-        {
-            get { return this; }
-        }
-
-        public string Id
-        {
-            get { return PID; }
-        }
-
-        private void UrlBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (UrlBox.Text.IndexOf(".myjetbrains.com", StringComparison.OrdinalIgnoreCase) >= 0 &&
-                !(UrlBox.Text.EndsWith("/youtrack", StringComparison.OrdinalIgnoreCase) ||
-                  UrlBox.Text.EndsWith("/youtrack/", StringComparison.OrdinalIgnoreCase)))
-            {
-                UrlBox.Text += "/youtrack";
-            }
-        }
-
-        private void TestConnection_Click(object sender, RoutedEventArgs e)
-        {
-            var validator = new YouTrackConnectionValidator();
-            var errorMessage = "";
-            if (!validator.TryValidateConnection(new ReTrackSettings
-            {
-                Username = UsernameBox.Text,
-                Password = PasswordBox.Password,
-                Url = UrlBox.Text
-            }, out errorMessage))
-            {
-                MessageBox.Show(
-                    errorMessage,
-                    "Test Connection", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            else
-            {
-                MessageBox.Show(
-                    string.Format("Successfully connected to {0}.", UrlBox.Text),
-                    "Test Connection", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-        }
+      //ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackUsername, UsernameBox, TextBox.TextProperty);
+      //ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackPassword, PasswordBox, TextBox.TextProperty);
+      //ctx.SetBinding(lifetime, (ReTrackSettingsReSharper s) => s.YouTrackUrl, UrlBox, TextBox.TextProperty);
     }
+
+    public bool OnOk()
+    {
+      return true;
+    }
+
+    public bool ValidatePage()
+    {
+      // todo: attempt to connect to source (if specified)
+      return true;
+    }
+
+    public EitherControl Control
+    {
+      get { return this; }
+    }
+
+    public string Id
+    {
+      get { return PID; }
+    }
+
+    private void UrlBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+      if (UrlBox.Text.IndexOf(".myjetbrains.com", StringComparison.OrdinalIgnoreCase) >= 0 &&
+          !(UrlBox.Text.EndsWith("/youtrack", StringComparison.OrdinalIgnoreCase) ||
+            UrlBox.Text.EndsWith("/youtrack/", StringComparison.OrdinalIgnoreCase)))
+      {
+        UrlBox.Text += "/youtrack";
+      }
+    }
+
+    private void TestConnection_Click(object sender, RoutedEventArgs e)
+    {
+      var validator = new YouTrackConnectionValidator();
+      var errorMessage = "";
+      if (!validator.TryValidateConnection(new ReTrackSettings
+      {
+        Username = UsernameBox.Text,
+        Password = PasswordBox.Password,
+        Url = UrlBox.Text
+      }, out errorMessage))
+      {
+        MessageBox.Show(
+            errorMessage,
+            "Test Connection", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      else
+      {
+        MessageBox.Show(
+            string.Format("Successfully connected to {0}.", UrlBox.Text),
+            "Test Connection", MessageBoxButton.OK, MessageBoxImage.Information);
+      }
+    }
+
+    public bool IsPortValueAllowed(string text)
+    {
+      byte s;
+      return byte.TryParse(text, out s);
+    }
+
+    /// <summary>
+    /// Prevent entry of non-numeric data.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void PortBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+      e.Handled = !IsPortValueAllowed(e.Text);
+    }
+
+    /// <summary>
+    /// Prevent pasting of non-numeric data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void PortBox_OnPasting(object sender, DataObjectPastingEventArgs e)
+    {
+      if (e.DataObject.GetDataPresent(typeof(String)))
+      {
+        var text = (String)e.DataObject.GetData(typeof(String));
+        if (!IsPortValueAllowed(text))
+        {
+          e.CancelCommand();
+        }
+      }
+      else
+      {
+        e.CancelCommand();
+      }
+    }
+  }
 }
